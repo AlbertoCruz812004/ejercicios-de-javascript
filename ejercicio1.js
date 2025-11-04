a9ffcb42f157560bd5c625ab160d8ccd11e0f2fd
jose alberto cruz martinez <albertocruz8133@hotmail.com>
2025-11-03 22:41:53 -0600

    refactor: Mueve el README principal al directorio de ejercicios de javascript

--- ejercicio1.js
+++
@@ -1,7 +0,0 @@
-//1. Pedir nombre y edad y mostrar en consola esos datos.
-
-const name = prompt("Escribe tu nombre: ");
-const age = prompt("Escribe tu edad: ");
-
-console.log(`Nombre: ${name}`);
-console.log(`Edad: ${age}`);
